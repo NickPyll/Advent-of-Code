36b55cc0d2ae39fdf31cc38e0f9385065e558db8
Nicholas Pylypiw <nick.pylypiw@gmail.com>
2023-12-02 10:02:54 -0500

    Update Day2_Cube_Conundrum.R
    just cleaning stuff up

--- 2023 Challenge/Day2_Cube_Conundrum.R
+++ 2023 Challenge/Day2_Cube_Conundrum.R
@@ -8,7 +8,7 @@
 
 library(tidyverse)
 
-input <- read.csv('data/input_day2.txt', sep = ";", header = FALSE) 
+input <- read.csv('data/input_day2.txt', sep = ";", header = FALSE)
 
 df <-
   input |> 
@@ -21,47 +21,33 @@ df <-
   filter(!is.na(value), value != '') |>
   separate('value', c('drop', 'count', 'color'), sep = ' ') |>
   select(-drop) |>
-  mutate(count = as.numeric(count)) |> 
+  mutate(count = as.numeric(count)) 
+
+df |> 
   mutate(
     red_exclude = if_else(color == 'red' & count > 12, 1, 0),
     green_exclude = if_else(color == 'green' & count > 13, 1, 0),
-    blue_exclude = if_else(color == 'blue' & count > 14, 1, 0)
-  ) |> 
-  mutate(exclude = red_exclude + green_exclude + blue_exclude) |> 
+    blue_exclude = if_else(color == 'blue' & count > 14, 1, 0),
+    exclude = red_exclude + green_exclude + blue_exclude) |> 
   group_by(GameID) |>
   summarize(exclude = max(exclude)) |>
   ungroup() |> 
   mutate(GameID_num = as.numeric(gsub("[^0-9.-]", "", GameID))) |> 
-  filter(exclude == 0) 
-
-df |> 
+  filter(exclude == 0) |> 
   summarise(sum = sum(GameID_num)) |> 
   pull()
 
 # Day 2 Cube Conundrum----
 ## Part 2 ----
 # Find minimum number of cubes that each game would require
-# Multiply those numbers together
+# Multiply those numbers together for each game (power)
 # Sum for entire set
 
-df <-
-  input |> 
-  separate(V1, c('GameID', 'V1'), ":") |> 
-  pivot_longer(!GameID) |>
-  select(-name) |>
-  separate('value', paste("Value", 1:3, sep = "_"), sep = ",", extra = "drop") |>
-  pivot_longer(!GameID) |>
-  select(-name) |>
-  filter(!is.na(value), value != '') |>
-  separate('value', c('drop', 'count', 'color'), sep = ' ') |>
-  select(-drop) |>
-  mutate(count = as.numeric(count)) |> 
+df |> 
   group_by(GameID, color) |>
   summarize(count = max(count)) |>
   ungroup() |> 
   pivot_wider(names_from = color, values_from = count) |> 
-  mutate(power = red*green*blue)
-
-df |> 
+  mutate(power = red*green*blue) |> 
   summarise(sum = sum(power)) |> 
   pull()
